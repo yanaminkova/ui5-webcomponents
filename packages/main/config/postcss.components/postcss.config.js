bce8cb93d7050f15d867866638f0dd3839ff3549
Petar Skelin <petar.skelin@sap.com>
2019-07-19 11:31:44 +0200

    initial impl

--- packages/main/config/postcss.components/postcss.config.js
+++ packages/main/config/postcss.components/postcss.config.js
@@ -1,20 +1,22 @@
 const postcssImport = require('postcss-import');
 const postcssAddFallback = require('../../lib/postcss-add-fallback/index.js');
 const postcssCSStoESM = require('../../lib/postcss-css-to-esm/index.js');
+const postcssGenerateClasses = require('../../lib/postcss-generate-classes/index.js');
 const cssnano = require('cssnano');
 
 module.exports = {
 	plugins: [
 		postcssImport(),
 		postcssAddFallback({importFrom: './dist/css/themes/sap_fiori_3/parameters-bundle.css'}),
-		cssnano({
-			preset: [
-				'default', {
-					mergeLonghand: false, // https://github.com/cssnano/cssnano/issues/675
-					mergeRules: false, // https://github.com/cssnano/cssnano/issues/730
-				},
-			]
-		}),
+		// cssnano({
+		// 	preset: [
+		// 		'default', {
+		// 			mergeLonghand: false, // https://github.com/cssnano/cssnano/issues/675
+		// 			mergeRules: false, // https://github.com/cssnano/cssnano/issues/730
+		// 		},
+		// 	]
+		// }),
 		postcssCSStoESM(),
+		postcssGenerateClasses(),
 	]
 };
